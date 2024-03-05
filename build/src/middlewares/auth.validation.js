d8a8171142383b0dda6a0f49b9ba8238254618f1
Shlomi Elbaz <shlomi.elbaz@outlook.com>
2024-03-05 17:36:55 +0200

    update .gitignore

--- build/src/middlewares/auth.validation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isAuth = void 0;
-function isAuth(req, res, next) {
-    console.log(`Authorized router ${req.originalUrl}`);
-    next();
-}
-exports.isAuth = isAuth;
--- build/src/routes/home.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const home_controller_1 = require("../controllers/home.controller");
-const router = (0, express_1.Router)();
-router.get("/", home_controller_1.welcome);
-router.get("/login", home_controller_1.login);
-router.get("/register", home_controller_1.register);
-exports.default = router;
